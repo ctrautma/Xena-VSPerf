94b9ff6616e5835cedd685d6776c85cfdd346d53
Christian Trautman <ctrautma@redhat.com>
2016-02-29 14:58:16 -0500

    Complete functional rewrite of XenaXML. More straight forward in its usage now.

--- XenaXML.py
+++ XenaXML.py
@@ -13,129 +13,131 @@
 # limitations under the License.
 
 import base64
-import binascii
 import json
 import logging
-import socket
-import struct
+
+import scapy.layers.inet as inet
 
 _logger = logging.getLogger(__name__)
 
-# Profile assignment as per generic config file
-ETHERNET_PROFILE = './profiles/Ethernet.x2544'
-ETHERNET_VLAN_PROFILE = './profiles/Ethernet_VLAN.x2544'
-ETHERNET_VLAN_IP_PROFILE = './profiles/Ethernet_VLAN_IPV4.x2544'
-ETHERNET_IP_PROFILE = './profiles/Ethernet_IPV4.x2544'
-
-# protocol numbers
-PROTOCOL_NUM = {6: 'TCP',
-                17: 'UDP',
-                255: 'RESERVED'}
-
-# TODO Do the VLAN Profile classes
 # TODO Document (docstring) and comment this file
 
 
 class XMLConfig(object):
-    def __init__(self, xmlpath):
+    def __init__(self, xml_path='./profiles/baseconfig.x2544'):
 
-        self._xmlpath = xmlpath
-        self._xmlDataPath = {
-            'Chassis_IP': ('ChassisManager', 'ChassisList', 0, 'HostName'),
-            'Chassis_Password': ('ChassisManager', 'ChassisList', 0,
-                                 'Password'),
-            'Duration': ('TestOptions', 'TestTypeOptionMap', 'Throughput',
-                         'Duration'),
-            'LossRate': ('TestOptions', 'TestTypeOptionMap', 'Throughput',
-                         'RateIterationOptions', 'AcceptableLoss'),
-            'Module1': ('PortHandler', 'EntityList', 0, 'PortRef',
-                        'ModuleIndex'),
-            'Module2': ('PortHandler', 'EntityList', 1, 'PortRef',
-                        'ModuleIndex'),
-            'Packet_Header': ('StreamProfileHandler', 'EntityList', 0,
-                              'StreamConfig', 'HeaderSegments'),
-            'Port1': ('PortHandler', 'EntityList', 0, 'PortRef', 'PortIndex'),
-            'Port1UID': ('PortHandler', 'EntityList', 0, 'ItemID'),
-            'Port2': ('PortHandler', 'EntityList', 1, 'PortRef', 'PortIndex'),
-            'Port2UID': ('PortHandler', 'EntityList', 1, 'ItemID'),
-            'Trials': ('TestOptions', 'TestTypeOptionMap', 'Throughput',
-                       'Iterations')}
+        self.xml_path = xml_path
 
-        self._filedata = dict()
+        self.file_data = dict()
 
         # Test config info
-        self._trials = None
-        self._duration = None
-        self._lossrate = None
+        self.trials = None
+        self.duration = None
+        self.loss_rate = None
 
         # Xena Chassis info
-        self._chassisIP = None
-        self._chassisPwd = None
+        self.chassisIP = None
+        self.chassisPwd = None
 
         # Physical Xena info
-        self._module1 = None
-        self._module2 = None
-        self._port1 = None
-        self._port2 = None
-        self._port1UID = None
-        self._port2UID = None
-        self._chassisID = None
+        self.module1 = None
+        self.module2 = None
+        self.port1 = None
+        self.port2 = None
+        self.port1_UID = None
+        self.port2_UID = None
+        self.chassisID = None
 
-        self._srcIP = None
-        self._dstIP = None
-        self._l4protocol = None
-        self._vLANTag = None
-        self._vLANid = None
-        self._vLANPriority = None
+        # header info
+        self.l2 = None
+        self.l3 = None
+        self.vlan = None
+        self.segments = list()
 
         # Read the xml file and configuration settings
         self.read_file()
         self.read_config()
 
-    @staticmethod
-    def decode_bytearray(encstr):
-        """ Decodes the base64-encoded string to a byte array
-            :param encstr: The base64-encoded string representing a byte array
-            :return: The decoded byte array
-        """
-        decstring = base64.b64decode(encstr)
-        b = bytearray()
-        b.extend(decstring)
-        return b
+    def add_header_segments(self):
+        packet = self.create_packet_header()
+        header_pos = 0
+        if self.l2:
+            value = str(encode_byte_array(bytes(packet)[:len(self.l2)]))
+            d = {"SegmentType": "ETHERNET",
+                 "SegmentValue": value,
+                 "ItemID": "bdf7bd1c-4634-4fb5-909b-6200237e2647",
+                 "ParentID": "",
+                 "Label": ""}
+            self.segments.append(d)
+            header_pos += len(self.l2)
+        if self.vlan:
+            value = str(encode_byte_array(bytes(packet)[header_pos: len(
+                self.vlan) + header_pos]))
+            d = {"SegmentType": "VLAN",
+                 "SegmentValue": value,
+                 "ItemID": "51af8770-99c4-4824-885d-990258e2a890",
+                 "ParentID": "",
+                 "Label": ""}
+            self.segments.append(d)
+            header_pos += len(self.vlan)
+        if self.l3:
+            value = str(encode_byte_array(bytes(packet)[header_pos: len(
+                self.l3) + header_pos]))
+            d = {"SegmentType": "IP",
+                 "SegmentValue": value,
+                 "ItemID": "1f67026e-0a83-462f-9c43-dd3661754167",
+                 "ParentID": "",
+                 "Label": ""}
+            self.segments.append(d)
+            header_pos += len(self.l3)
 
-    @staticmethod
-    def encode_bytearray(bytearr):
-        """ Encodes the byte array as a base64-encoded string
-        :param bytearr: A bytearray containing the bytes to convert
-        :return: A base64 encoded string
-        """
-        encstring = base64.b64encode(bytes(bytearr))
-        return encstring
+    def build_l2_header(self, dst_mac='aa:aa:aa:aa:aa:aa',
+                        src_mac='bb:bb:bb:bb:bb:bb', **kwargs):
+        self.l2 = inet.Ether(dst=dst_mac, src=src_mac, **kwargs)
+
+    def build_l3_header_ip4(self, src_ip='192.168.0.2', dst_ip='192.168.0.3',
+                            protocol='UDP', **kwargs):
+        self.l3 = inet.IP(src=src_ip, dst=dst_ip, proto=protocol.lower(),
+                          **kwargs)
+
+    def build_vlan_header(self, vlan_id=1, **kwargs):
+        self.vlan = inet.Dot1Q(vlan=vlan_id, **kwargs)
+
+    def create_packet_header(self):
+        packet = inet.Ether()
+        if self.l2:
+            packet = self.l2
+        if self.vlan:
+            packet /= self.vlan
+        if self.l3:
+            packet /= self.l3
+        return packet
 
     def read_config(self):
         try:
-            self._chassisIP = self._filedata.lookup(
-                *self._xmlDataPath['Chassis_IP'])
-            self._chassisPwd = self._filedata.lookup(
-                *self._xmlDataPath['Chassis_Password'])
-            self._duration = self._filedata.lookup(
-                *self._xmlDataPath['Duration'])
-            self._lossrate = self._filedata.lookup(
-                *self._xmlDataPath['LossRate'])
-            self._module1 = self._filedata.lookup(
-                *self._xmlDataPath['Module1'])
-            self._module2 = self._filedata.lookup(
-                *self._xmlDataPath['Module2'])
-            self._port1 = self._filedata.lookup(
-                *self._xmlDataPath['Port1'])
-            self._port1UID = self._filedata.lookup(
-                *self._xmlDataPath['Port1UID'])
-            self._port2 = self._filedata.lookup(
-                *self._xmlDataPath['Port2'])
-            self._port2UID = self._filedata.lookup(
-                *self._xmlDataPath['Port2UID'])
-            self._trials = self._filedata.lookup(
-                *self._xmlDataPath['Trials'])
+            self.chassisIP = self.file_data['ChassisManager']['ChassisList'][
+                0]['HostName']
+            self.chassisPwd = self.file_data['ChassisManager'][
+                'ChassisList'][0]['Password']
+            self.duration = self.file_data['TestOptions'][
+                'TestTypeOptionMap']['Throughput']['Duration']
+            self.loss_rate = self.file_data['TestOptions'][
+                'TestTypeOptionMap']['Throughput']['RateIterationOptions'][
+                'AcceptableLoss']
+            self.module1 = self.file_data['PortHandler']['EntityList'][0][
+                'PortRef']['ModuleIndex']
+            self.module2 = self.file_data['PortHandler']['EntityList'][1][
+                'PortRef']['ModuleIndex']
+            self.port1 = self.file_data['PortHandler']['EntityList'][0][
+                'PortRef']['PortIndex']
+            self.port1_UID = self.file_data['PortHandler']['EntityList'][0][
+                'ItemID']
+            self.port2 = self.file_data['PortHandler']['EntityList'][1][
+                'PortRef']['PortIndex']
+            self.port2_UID = self.file_data['PortHandler']['EntityList'][1][
+                'ItemID']
+            self.trials = self.file_data['TestOptions']['TestTypeOptionMap'][
+                'Throughput']['Iterations']
             return True
         except Exception as e:
             _logger.exception(
@@ -143,269 +145,77 @@ class XMLConfig(object):
             return False
 
     def read_file(self):
-        # RUINED!
         try:
-            with open(self._xmlpath, 'r', encoding='utf-8') as data_file:
-                self._filedata = MyDict(json.loads(data_file.read()))
+            with open(self.xml_path, 'r', encoding='utf-8') as data_file:
+                self.file_data = json.loads(data_file.read())
                 return True
         except Exception as e:
             _logger.exception('Exception during Xena xml read: {}'.format(e))
             return False
 
-    def write_file(self, outputpath):
+    def write_config(self):
+        self.file_data['ChassisManager']['ChassisList'][0][
+            'HostName'] = self.chassisIP
+        self.file_data['ChassisManager']['ChassisList'][0][
+            'Password'] = self.chassisPwd
+        self.file_data['TestOptions']['TestTypeOptionMap']['Throughput'][
+            'Duration'] = self.duration
+        self.file_data['TestOptions']['TestTypeOptionMap']['Throughput'][
+            'RateIterationOptions']['AcceptableLoss'] = self.loss_rate
+        self.file_data['PortHandler']['EntityList'][0]['PortRef'][
+            'ModuleIndex'] = self.module1
+        self.file_data['PortHandler']['EntityList'][1]['PortRef'][
+            'ModuleIndex'] = self.module2
+        self.file_data['PortHandler']['EntityList'][0]['PortRef'][
+            'PortIndex'] = self.port1
+        self.file_data['PortHandler']['EntityList'][0][
+            'ItemID'] = self.port1_UID
+        self.file_data['PortHandler']['EntityList'][1]['PortRef'][
+            'PortIndex'] = self.port2
+        self.file_data['PortHandler']['EntityList'][1][
+            'ItemID'] = self.port2_UID
+        self.file_data['TestOptions']['TestTypeOptionMap']['Throughput'][
+            'Iterations'] = self.trials
+        self.file_data['StreamProfileHandler']['EntityList'][0][
+            'StreamConfig']['HeaderSegments'] = self.segments
+
+    def write_file(self, output_path):
         try:
-            with open(outputpath, 'w',encoding='utf-8') as f:
-                json.dump(self._filedata, f, indent=2, sort_keys=True,
+            with open(output_path, 'w', encoding='utf-8') as f:
+                json.dump(self.file_data, f, indent=2, sort_keys=True,
                           ensure_ascii=True)
             return True
         except Exception as e:
             _logger.exception('Exception during Xena xml write: {}'.format(e))
             return False
 
-    @property
-    def chassis_ip(self):
-        return self._chassisIP
 
-    @chassis_ip.setter
-    def chassis_ip(self, ip):
-        self._chassisIP = ip
-        set_path(self._filedata, self._xmlDataPath['Chassis_IP'], ip)
-
-    @property
-    def chassis_pwd(self):
-        return self._chassisPwd
-
-    @chassis_pwd.setter
-    def chassis_pwd(self, pwd):
-        self._chassisPwd = pwd
-        set_path(self._filedata, self._xmlDataPath['Chassis_Password'], pwd)
-
-    @property
-    def duration(self):
-        return self._duration
-
-    @duration.setter
-    def duration(self, numint):
-        self._duration = numint
-        set_path(self._filedata, self._xmlDataPath['Duration'], numint)
-
-    @property
-    def lossrate(self):
-        return self._lossrate
-
-    @lossrate.setter
-    def lossrate(self, numfloat):
-        self._lossrate = numfloat
-        set_path(self._filedata, self._xmlDataPath['LossRate'], numfloat)
-
-    @property
-    def port1(self):
-        return self._port1
-
-    @port1.setter
-    def port1(self, port):
-        self._port1 = port
-        set_path(self._filedata, self._xmlDataPath['Port1'], port)
-
-    @property
-    def port1UID(self):
-        return self._port1UID
-
-    @property
-    def port2(self):
-        return self._port2
-
-    @port2.setter
-    def port2(self, port):
-        self._port2 = port
-        set_path(self._filedata, self._xmlDataPath['Port2'], port)
-
-    @property
-    def port2UID(self):
-        return self._port2UID
-
-    @property
-    def trials(self):
-        return self._trials
-
-    @trials.setter
-    def trials(self, numint):
-        self._trials = numint
-        set_path(self._filedata, self._xmlDataPath['Trials'], numint)
+def decode_byte_array(enc_str):
+    """ Decodes the base64-encoded string to a byte array
+        :param enc_str: The base64-encoded string representing a byte array
+        :return: The decoded byte array
+    """
+    dec_string = base64.b64decode(enc_str)
+    b = bytearray()
+    b.extend(dec_string)
+    return b
 
 
-class XMLEther(XMLConfig):
-    def __init__(self, *args, **kwargs):
-        XMLConfig.__init__(self, ETHERNET_PROFILE, *args, **kwargs)
+def encode_byte_array(byte_arr):
+    """ Encodes the byte array as a base64-encoded string
+    :param byte_arr: A bytearray containing the bytes to convert
+    :return: A base64 encoded string
+    """
+    enc_string = base64.b64encode(bytes(byte_arr))
+    return enc_string
 
-        self._xmlDataPath['EthernetHeader'] = (
-            self._xmlDataPath['Packet_Header'] + (0, 'SegmentValue'))
-
-        # packet info
-        self._ethernetheader = self.ethernetheader
-        self._srcmac = self.srcmac
-        self._dstmac = self.dstmac
-
-    @property
-    def dstmac(self):
-        barray = self.decode_bytearray(self.ethernetheader)
-        flatstring = binascii.hexlify(bytes(barray[6:12])).decode('utf-8')
-        myiter = iter(flatstring)
-        stringwithcolons = ':'.join(a+b for a, b in zip(myiter, myiter))
-        return stringwithcolons
-
-    @dstmac.setter
-    def dstmac(self, macaddr):
-        macaddr = macaddr.split(':')
-        barray = self.decode_bytearray(self.ethernetheader)
-        for i in range(0, 6):
-            barray[i + 6] = int(macaddr[i], 16)
-        enc64 = self.encode_bytearray(barray)
-        set_path(self._filedata, self._xmlDataPath[
-            'EthernetHeader'], enc64.decode('utf-8'))
-        self._dstmac = macaddr
-
-    @property
-    def ethernetheader(self):
-        return self._filedata.lookup(*self._xmlDataPath['EthernetHeader'])
-
-    @property
-    def srcmac(self):
-        barray = self.decode_bytearray(self.ethernetheader)
-        flatstring = binascii.hexlify(bytes(barray[0:6])).decode('utf-8')
-        myiter = iter(flatstring)
-        stringwithcolons = ':'.join(a+b for a, b in zip(myiter, myiter))
-        return stringwithcolons
-
-    @srcmac.setter
-    def srcmac(self, macaddr):
-        macaddr = macaddr.split(':')
-        barray = self.decode_bytearray(self.ethernetheader)
-        for i in range(0, 6):
-            barray[i] = int(macaddr[i], 16)
-        enc64 = self.encode_bytearray(barray)
-        set_path(self._filedata, self._xmlDataPath[
-            'EthernetHeader'], enc64.decode('utf-8'))
-        self._srcmac = macaddr
-
-
-class XMLEtherIP(XMLEther):
-    def __init__(self, *args, **kwargs):
-        XMLConfig.__init__(self, ETHERNET_IP_PROFILE, *args, **kwargs)
-
-        self._xmlDataPath['EthernetHeader'] = (
-            self._xmlDataPath['Packet_Header'] + (0, 'SegmentValue'))
-        self._xmlDataPath['IPHeader'] = (
-            self._xmlDataPath['Packet_Header'] + (1, 'SegmentValue'))
-
-        # packet info
-        self._ethernetheader = self.ethernetheader
-        self._ipheader = self.ipheader
-        self._srcmac = self.srcmac
-        self._dstmac = self.dstmac
-        self._dstIP = self._dstIP
-        self._srcIP = self._srcIP
-        self._l4proto = self.l4proto
-
-    @property
-    def dstIP(self):
-        barray = self.decode_bytearray(self.ipheader)
-        flatstring = binascii.hexlify(bytes(barray[16:20])).decode('utf-8')
-        addr_long = int(flatstring, 16)
-        return socket.inet_ntoa(struct.pack("!L", addr_long))
-
-    @dstIP.setter
-    def dstIP(self, ipaddr):
-        octets = ipaddr.split('.')
-        barray = self.decode_bytearray(self.ipheader)
-        for i in range(0, 4):
-            barray[i + 16] = int(octets[i])
-        enc64 = self.encode_bytearray(barray)
-        set_path(self._filedata, self._xmlDataPath[
-            'IPHeader'], enc64.decode('utf-8'))
-        self._dstIP = ipaddr
-
-    @property
-    def ipheader(self):
-        return self._filedata.lookup(*self._xmlDataPath['IPHeader'])
-
-    @property
-    def l4proto(self):
-        barray = self.decode_bytearray(self.ipheader)
-        value = barray[9]
-        if value not in PROTOCOL_NUM.keys():
-            _logger.error(
-                'Protocol number {} in L3 not found in XenaXML Proto var.')
-            return None
-        else:
-            return PROTOCOL_NUM[barray[9]]
-
-    @l4proto.setter
-    def l4proto(self, protocol):
-        protocol = protocol.upper()
-
-        # reverse the key values in the PROTOCOL_NUM and get the int value
-        res = dict((v, k) for k, v in PROTOCOL_NUM.items())
-        value = res[protocol]
-
-        barray = self.decode_bytearray(self.ipheader)
-        barray[9] = value
-        enc64 = self.encode_bytearray(barray)
-        set_path(self._filedata, self._xmlDataPath[
-            'IPHeader'], enc64.decode('utf-8'))
-        self._l4proto = protocol
-
-    @property
-    def srcIP(self):
-        barray = self.decode_bytearray(self.ipheader)
-        flatstring = binascii.hexlify(bytes(barray[12:16])).decode('utf-8')
-        addr_long = int(flatstring, 16)
-        return socket.inet_ntoa(struct.pack("!L", addr_long))
-
-    @srcIP.setter
-    def srcIP(self, ipaddr):
-        octets = ipaddr.split('.')
-        barray = self.decode_bytearray(self.ipheader)
-        for i in range(0, 4):
-            barray[i + 12] = int(octets[i])
-        enc64 = self.encode_bytearray(barray)
-        set_path(self._filedata, self._xmlDataPath[
-            'IPHeader'], enc64.decode('utf-8'))
-        self._srcIP = ipaddr
-
-
-class MyDict(dict):
-    def lookup(self, *args):
-        tmp = self
-        for _ in args:
-            tmp = tmp[_]
-        return tmp
-
-
-def set_path(somedict, path, value):
-    comstr = "somedict"
-    for p in path:
-        if type(p) == str:
-            comstr = "{}['{}']".format(comstr, p)
-        else:
-            comstr = "{}[{}]".format(comstr, p)
-    if type(value) == str:
-        exec("{} = '{}'".format(comstr, value))
-    else:
-        exec("{} = {}".format(comstr, value))
 
 if __name__ == "__main__":
     print("Running UnitTest for XenaXML")
-    x = XMLEtherIP()
-    x.trials = 3
-    print(x.srcmac, x.dstmac)
-    x.srcmac, x.dstmac = ('cc:cc:cc:cc:cc:cc', 'dd:dd:dd:dd:dd:dd')
-    print(x.srcmac, x.dstmac)
-    print(x.srcIP, x.dstIP)
-    x.srcIP, x.dstIP = ('192.168.100.10', '192.168.100.11')
-    print(x.srcIP, x.dstIP)
-    print(x.l4proto)
-    x.l4proto = 'udp'
-    print(x.l4proto)
-    x.write_file('./profiles/test.x2544')
+    x = XMLConfig()
+    x.build_l2_header()
+    x.build_l3_header_ip4()
+    x.add_header_segments()
+    x.write_config()
+    x.write_file('./testthis.x2544')
 
