bf1a08db2090b1d7427bdda00659d381f896f4a1
Amit Supugade <asupugad@redhat.com>
2018-09-24 10:18:41 -0400

    Added run_xena script for make it easier to use XenaDriver.py

---
+++ tools/pkt_gen/xena/run_xena.py
@@ -0,0 +1,46 @@
+"""
+USAGE:
+- Use python 3
+- Run 'python run_xena --run module --speed 25'
+"""
+import argparse
+from XenaDriver import *
+
+
+def run(args):
+    PACKET_SIZE = 128
+    DURATION = 10
+    DRIVER = XenaSocketDriver('10.19.15.19')
+    X_MANAGER = XenaManager(DRIVER, 'vsperf', 'xena')
+    
+    run = str(args.run[0]).lower()
+    if run == "module":
+        speed = args.speed[0]
+        run_module(X_MANAGER, speed)
+    else:
+        pass
+    
+
+def run_module(X_MANAGER, speed):
+    if speed == 25:
+        media = "SFP28"
+    elif speed == 100:
+        media = "QSFP28"
+    else:
+        pass
+    
+    print("Changing speed to {}G".format(speed)) 
+    
+    X_MODULE = XenaModule(X_MANAGER, 9)
+    X_MODULE.change_media(media)
+    
+    print("Media Speed Changed to {}G".format(speed))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='To Access Xena Driver classes')
+    parser.add_argument('--run', nargs='+', type=str, help='Class to run from XenaDriver')
+    parser.add_argument('--speed', nargs=1, type=int, help='Module speed to change it to', required=True)
+   
+    args = parser.parse_args()
+    run(args) 
